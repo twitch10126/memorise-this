e4170be1e9fadf2e5fc40d4afeb8a681322e26a2
twitch10126 <twitch10126@hotmail.com>
2025-05-07 20:40:45 +0100

    Add game object to script.js

--- scripts/js/script.js
+++ scripts/js/script.js
@@ -1,1 +1,12 @@
-console.log("hello World")
+
+// when the document is fully loaded the script will run
+$(document).ready(function () {
+
+    let game = {
+        round: 1,
+        gameSequence: [],
+        userSequence: [],
+        buttons: ["red", "blue", "green", "purple"],
+    };
+
+});
