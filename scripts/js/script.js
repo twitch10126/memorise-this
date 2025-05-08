2f873932f9122c8e89ddc841fb6c2ddb69b35d25
twitch10126 <twitch10126@hotmail.com>
2025-05-08 09:17:11 +0100

    Change text to score:  while the game is running

--- scripts/js/script.js
+++ scripts/js/script.js
@@ -17,6 +17,7 @@ $(document).ready(function () {
         $("i").addClass("hidden");
         $(".overlay-text").addClass("hidden");
         $(".start").removeClass("border");
+        $(".start").text("Score:").css("font-size", "2em","width", "90%");
         console.log("Game has started");
     };
 
