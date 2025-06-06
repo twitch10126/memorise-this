99f28d480d6b1164f47212c7f106b6d2533452fc
twitch10126 <twitch10126@hotmail.com>
2025-06-06 23:00:35 +0100

    Add redirect functionality to 404.js

---
+++ scripts/js/404.js
@@ -0,0 +1,10 @@
+const redirect = [5, 4, 3, 2, 1];
+redirect.forEach((num, index) => {
+    setTimeout(() => {
+        $(".countdown").text(num);
+    }, index * 1000);
+});
+
+setTimeout(function () {
+    window.location.href = "index.html";
+}, 5000);
